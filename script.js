3fbfa3ec3fe7f9d7eceb1080e8301551f27356ea
Shailmanncoder <shailmmanncoder@gmail.com>
2024-12-18 15:54:44 +0530

    sir I am to late to push it

---
+++ script.js
@@ -0,0 +1,14 @@
+let a=parseInt(prompt('Enter age to check that you are eligible for voting :'))
+
+
+
+
+if (a>18) {
+    document.write("You are eligble for voting");
+}
+if (a<18) {
+    document.write("You are not eligble for voting");
+}
+if (a==18){
+    document.write("You are eligble for voting");
+}
