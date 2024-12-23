5ba7b92bab606afd531325332c0b1238b4014565
Shailmanncoder <shailmmanncoder@gmail.com>
2024-12-23 12:29:54 +0530

    Arrays concept

---
+++ script1.js
@@ -0,0 +1,12 @@
+let a=parseInt(prompt(('Enter first number :')))
+let b=parseInt(prompt(('Enter second number to check that it is greater or smaller :')))
+
+if(a>b){
+    document.write(a,' is greater then ',b)
+}
+if(a<b){
+    document.write(b,' is smaller then ',a)
+}
+if(a=b){
+    document.write(' Both number are equal : ',a=b,'=',a=b)
+}
